c51e8ca968761d1210f53f695f6b22fd4046e608
quantumke <nguruben@gmail.com>
2016-08-10 21:34:24 +0300

    mail save models

--- jobsboard/app/models.py
+++ jobsboard/app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class contactus(models.Model):
+    name=models.CharField(unique=False, max_length=100)
+    email=models.CharField(unique=False, max_length=100)
+    phone=models.CharField(unique=False, max_length=100)
+    message=models.TextField(unique=False, max_length=500)
+
+    def __unicode__(self):
+        return  self.name
+
+class prospects(models.Model):
+    name=models.CharField(max_length=100, unique=False)
+    email=models.CharField(max_length=100, unique=False)
+    phone=models.CharField(max_length=100, unique=False)
+    package=models.CharField(max_length=100, unique=False)
+    description=models.CharField(max_length=100, unique=False)
+
+    def __unicode__(self):
+        return self.name
+
